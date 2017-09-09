4fe3c6bcc5ebcb8a16c44308dc022ee41648d5e4
Tsukasa Sumizono <freddiesumiz.3@gmail.com>
2017-09-10 02:18:44 +0900

    Enable to play Ads movie.

---
+++ Assets/Scripts/TitleButtonManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class TitleButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		//UnityAdsの初期化
+		if(Advertisement.isSupported)
+		{
+			Advertisement.Initialize ("1538243");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void MovieButton()
+	{
+		AdsPlay ();
+	}
+
+	//広告の表示を行う関数
+	void AdsPlay()
+	{
+		if(Advertisement.IsReady())
+		{
+			Advertisement.Show ();
+		}
+	}
+}
